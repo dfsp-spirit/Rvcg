35c7b5f03fd51fb308f89f17b97528037be20a30
Tim Schäfer <ts+code@rcmd.org>
2021-06-26 16:07:36 +0200

    fix onering function

--- R/vcgOneRing.r
+++ R/vcgOneRing.r
@@ -3,18 +3,28 @@
 #'
 #' @param x tmesh3d instance
 #'
-#' @return list of integer vectors, the neighborhoods. Curently this function still returns FACE indices.
+#' @param vi optional, vector of positive vertex indices for which to compute neighborhood, all are used if left at \code{NULL}.
+#'
+#' @return list of integer vectors, the neighborhoods.
 #'
 #' @examples
 #' data(humface)
 #' neighborhoods <- vcgOneRing(humface)
+#' \dontrun{
+#'   if(requireNamespace("fsbrain", quitely=TRUE)) {
+#'   sjd = fsaverage.path(TRUE);
+#'   surface = subject.surface(sjd, 'fsaverage', surface = "white", hemi = "lh");
+#'   neigh = vcgOneRing(fs.surface.to.tmesh3d(surface));
+#'   fsbrain::highlight.vertices.on.subject(sjd, 'fsaverage', verts_lh=neigh[[100]]);
+#'   # assert max(unlist(lapply(neigh, max))) == 1643842 }
 #'
 #' @export
-vcgOneRing <- function(x) {
+vcgOneRing <- function(x, vi=NULL, numstep=1L) {
+  if(is.null(vi)) {
+    vi = seq(ncol(x$vb));
+  }
+  vi <- as.integer(vi - 1L)
   vb <- x$vb
   it <- x$it - 1L
-  out <- .Call("RVFadj",vb,it)
-  return(out)
-  # neigh = out
-  # return(unique(as.integer(surface$faces[neigh[[1000]],])))
+  out <- .Call("RVVadj",vb,it,vi,numstep)
 }
